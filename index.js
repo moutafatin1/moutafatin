3577f5711aa3d9c51e15df8965b82af580f73614
Oussama Moutafatin <moutafatin3@gmail.com>
2022-10-23 08:50:40 +0100

    feat: add welcome header

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 #!/usr/bin/env node
-import clearConsole from "clear-any-console";
+import welcome from "cli-welcome";
+import pkgJson from "./package.json" assert { type: "json" };
 
-clearConsole();
+welcome({
+  title: pkgJson.name,
+  tagLine: "Get to know Moutafatin",
+  description: pkgJson.description,
+  version: pkgJson.version,
+  clear: true,
+  bgColor: "#A020F0",
+  color: "#fff",
+  bold: true,
+});
+
 console.log(`
 Oussama Moutafatin
 
